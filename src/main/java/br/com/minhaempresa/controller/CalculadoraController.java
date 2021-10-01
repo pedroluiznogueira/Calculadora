868d459c4cd8df7ea70b17e2df9731bc91d6f22c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-01 09:39:43 -0300

    Sobre escrevendo doPost()

--- src/main/java/br/com/minhaempresa/controller/CalculadoraController.java
+++ src/main/java/br/com/minhaempresa/controller/CalculadoraController.java
@@ -1,5 +1,6 @@
 package br.com.minhaempresa.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,5 +11,8 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/CalculadoraServlet")
 public class CalculadoraController extends HttpServlet {
 
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
